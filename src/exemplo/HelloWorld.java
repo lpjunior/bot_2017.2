950d30b0f5ddfd722ae2b54369ae58324e7c46f7
luis.junior <luis.junior@47DE195361.senacrjedu.local>
2018-03-29 12:11:27 -0300

    Projeto hello world

---
+++ src/exemplo/HelloWorld.java
@@ -0,0 +1,16 @@
+package exemplo;
+
+/**
+ * @author: Luis Paulo
+ * @since: 2018-03-29
+ * @version: 1.0
+ * @category: Exemplo de classe
+ */
+public class HelloWorld {
+
+	// método de arranque(execução) do Java SE
+	public static void main(String[] args) {
+		/* Método de saída(print) de dados */ 
+		System.out.println("Olá Mundo Java!");
+	}
+}
---
+++ src/exemplo/Variaveis.java
@@ -0,0 +1,37 @@
+package exemplo;
+
+public class Variaveis {
+	public static void main(String[] args) {
+		/* Variáveis do tipo primitivo */
+		
+		// Para valores inteiros temos:
+		int inteiro = 10;
+		long inteiroMaior = 100000L;
+		
+		// Para valores reais temos:
+		float real = 10.0f;
+		double realMaior = 30.11;
+		
+		// Para valores letras temos:
+		char letra = 'L';
+		
+		// Para valores booleanos temos:
+		boolean booleano = true; // ou false
+		
+		/* Variáveis do tipo Wrapper (classe) */
+		
+		// Para valores inteiro temos:
+		Integer wInteiro = 30;
+		Long wInteiroMaior = 1000000l;
+		
+		// Para valores reais temos:
+		Float wReal = 10.0f;
+		Double wRealMaior = 31.0;
+		
+		// Para valores texto temos:
+		String wTexto = "Segunda aula de Java";
+		
+		// Para valores do tipo booleanos temos:
+		Boolean wBooleano = false;
+	}
+}
